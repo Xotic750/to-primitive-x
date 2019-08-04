3ae6cc7613bf849394d5b9c3207e2a6243972605
Graham Fairweather <xotic750@gmail.com>
2019-08-04 21:25:49 +0200

    Auto run

--- dist/to-primitive-x.js
+++ dist/to-primitive-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-08-04T14:09:32.343Z",
+  "date": "2019-08-04T19:25:44.147Z",
   "describe": "",
   "description": "Converts a JavaScript object to a primitive value.",
   "file": "to-primitive-x.js",
